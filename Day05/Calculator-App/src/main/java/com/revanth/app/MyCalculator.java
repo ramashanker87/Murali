7014f5b6bc0fbb3161450525dfc51bcda6cffc32
admin <rama@gail.com>
2025-02-13 09:22:15 -0800

    Day05

---
+++ Day05/Calculator-App/src/main/java/com/revanth/app/MyCalculator.java
@@ -0,0 +1,19 @@
+package com.revanth.app;
+
+public class MyCalculator {
+    public double add(double n1, double n2) {
+        return n1+n2;
+    }
+
+    public double sub(double n1,double n2){
+        return n1-n2;
+    }
+
+    public double div(double n1,double n2){
+        return n1/n2;
+    }
+
+    public double multiply(double n1,double n2){
+        return n1*n2;
+    }
+}
