508e244ad77661979c056a8b5eaff7a0a7f5b9bf
admin <rama@gail.com>
2025-02-13 09:20:45 -0800

    Day01

---
+++ Day01/first-java-project/src/main/java/com/rama/app/App.java
@@ -0,0 +1,14 @@
+package com.rama.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Hello World!" );
+    }
+}
