348c9dbf7948c4ef297aadec45aef99e8ded2756
admin <rama@gail.com>
2025-03-05 19:58:58 -0800

    Day 12

---
+++ Day12/patient-management-app/src/main/java/com/murali/app/service/PatientService.java
@@ -0,0 +1,37 @@
+package com.murali.app.service;
+
+import com.murali.app.model.Patient;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PatientService {
+        Map<String, Patient> patients=new HashMap<>();
+
+        public Patient createPatient(Patient patient){
+            patients.put(patient.getId(),patient);
+            return patient;
+        }
+
+        public Map<String,Patient> readAllPatient() {
+            return patients;
+        }
+
+        public Patient updatePatientHospital(String id,String newHospitalName){
+            Patient patient=patients.get(id);
+            patient.setHospitalName(newHospitalName);
+            return patient;
+        }
+
+        public void deletePatient(String id){
+            patients.remove(id);
+        }
+
+        public Patient readPatientByName(String name){
+            Patient result=patients.get(name);
+            return result;
+        }
+
+}
---
+++ Day12/patient-management-app/src/test/java/com/murali/app/PatientManagementAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.murali.app;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class PatientManagementAppApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//}
